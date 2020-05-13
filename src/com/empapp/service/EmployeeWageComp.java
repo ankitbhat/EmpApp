dda178c9ba5a65437c213787ce54f2923cde3e55
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-13 08:39:42 +0530

    Added condition to calculate wages till total working hours or days is reached for a month

--- src/com/empapp/service/EmployeeWageComp.java
+++ src/com/empapp/service/EmployeeWageComp.java
@@ -4,10 +4,11 @@ public class EmployeeWageComp
 {
 	int temp=0;
 	int tempWage=0;
+	int days=1,hours=0;
 	Employee emp = new Employee();
 	public void empCheck()
 	{
-		for(int i=0;i<20;i++)
+		while(days<21 && hours<100)
 		{
 			int empCheck = (int)Math.floor(Math.random() * 100) % 3;
 			switch(empCheck)
@@ -16,19 +17,26 @@ public class EmployeeWageComp
 					System.out.println("Employee is Present and Full Time");
 					System.out.println("Daily Employee Wage is :" + emp.dailyFullWage());
 					temp = emp.dailyFullWage();
+					hours+=emp.FULLTIMEHOURS;
 					break;
 				case 2:
 					System.out.println("Employee is Present and Part Time");
 					System.out.println("Daily Employee Wage is :" + emp.dailyPartWage());
 					temp = emp.dailyPartWage();
+					hours+=emp.PARTTIMEHOURS;
 					break;
 				case 0:
 					System.out.println("Employee is Absent");
 					temp = 0;
+					hours+=0;
 					break;
 			}
 			emp.setMonthlyWage(tempWage+=temp);
+			System.out.println("Total Hours :" + hours);
+			System.out.println("Total Days :" + days);
+			System.out.println("Monthly wage is :" + emp.getMonthlyWage());
+			days++;
 		}
-		System.out.println("Monthly wage is :" + emp.getMonthlyWage());
+		
 	}
 }
