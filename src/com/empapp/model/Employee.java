85d1fad037040226c4e64339716e9834f58d297d
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-13 08:23:24 +0530

    Added functionality to calculate monthly wage

--- src/com/empapp/model/Employee.java
+++ src/com/empapp/model/Employee.java
@@ -7,6 +7,7 @@ public static final int WAGEPERHOUR = 20;
 public static final int FULLTIMEHOURS = 8;
 public static final int PARTTIMEHOURS = 4;
 int dailyEmpWage =0;
+int monthlyWage = 0;
 	public int dailyFullWage()
 	{
 	dailyEmpWage =(FULLTIMEHOURS*WAGEPERHOUR);
@@ -16,5 +17,13 @@ int dailyEmpWage =0;
 	{
 	dailyEmpWage =(PARTTIMEHOURS*WAGEPERHOUR);
 	return dailyEmpWage;
+	}
+	public void setMonthlyWage(int monthlyWage)
+	{
+		this.monthlyWage = monthlyWage;
+	}
+	public int getMonthlyWage()
+	{
+	return this.monthlyWage;
 	}
 }
