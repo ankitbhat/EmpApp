efd855df1143e81db1a5163b8af1b025ee412be6
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-13 07:33:18 +0530

    Added functionality for part time employee and wage

--- src/com/empapp/service/EmployeeWageComp.java
+++ src/com/empapp/service/EmployeeWageComp.java
@@ -5,13 +5,18 @@ public class EmployeeWageComp
 	Employee emp = new Employee();
 	public void empCheck()
 	{
-	double empCheck = Math.floor(Math.random() * 10) % 2;
+	double empCheck = Math.floor(Math.random() * 100) % 3;
 		if(empCheck == emp.FULLTIME)
 		{
-			System.out.println("Employee is Present");
-			double dailyEmpWage =(emp.FULLTIMEHOURS*emp.WAGEPERHOUR);
-			System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+			System.out.println("Employee is Present and Full Time");
+			System.out.println("Daily Employee Wage is :" + emp.dailyFullWage());
 		}
+		else if(empCheck == emp.PARTTIME)
+		{
+			System.out.println("Employee is Present and Part Time");
+			System.out.println("Daily Employee Wage is :" + emp.dailyPartWage());
+		}
+		
 		else
 			System.out.println("Employee is Absent");
 	}
