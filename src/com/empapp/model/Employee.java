19cc0e7d058a2bf696125ca1b26fb178f67b6774
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-13 08:28:41 +0530

    Added functionality to calculate monthly wage

--- src/com/empapp/model/Employee.java
+++ src/com/empapp/model/Employee.java
@@ -1,13 +1,13 @@
 package com.empapp.model;
 public class Employee
 {
-public static final int FULLTIME = 1;
-public static final int PARTTIME = 2;
-public static final int WAGEPERHOUR = 20;
-public static final int FULLTIMEHOURS = 8;
-public static final int PARTTIMEHOURS = 4;
-int dailyEmpWage =0;
-int monthlyWage = 0;
+	public static final int FULLTIME = 1;
+	public static final int PARTTIME = 2;
+	public static final int WAGEPERHOUR = 20;
+	public static final int FULLTIMEHOURS = 8;
+	public static final int PARTTIMEHOURS = 4;
+	int dailyEmpWage =0;
+	int monthlyWage = 0;
 	public int dailyFullWage()
 	{
 		dailyEmpWage =(FULLTIMEHOURS*WAGEPERHOUR);
